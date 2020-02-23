b52d1fb7f9788f87d3a0644ec4cb364a08042dfb
Scott Ladd <scottjr101@gmail.com>
2020-02-22 20:54:28 -0500

    Setup context for state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, Component, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+const MyProvider = (props) => {
+
+const [age, setAge] = useState(100);
+
+    return (
+      <MyContext.Provider
+        value={{
+          state: setAge,
+          getLocation: () =>
+            setAge(
+              age + 1
+            )
+        }}
+      >
+        {props.children}
+      </MyContext.Provider>
+    );
 }
+
+const App = () => {
+  return (
+    <Fragment>
+      <MyProvider>
+        <MyContext.Consumer>
+          {context => (
+            <Fragment>
+              <p>Age: {context.age}</p>
+              <button onClick={context.getLocation}>Click Here</button>
+            </Fragment>
+          )}
+        </MyContext.Consumer>
+      </MyProvider>
+    </Fragment>
+  );
+};
 
 export default App;
