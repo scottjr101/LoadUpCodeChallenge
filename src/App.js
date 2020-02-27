4bfd8161cca26fd39c89864b063e7bfb8c9ab20d
Scott Ladd <scottjr101@gmail.com>
2020-02-27 17:29:49 -0500

    moved jumbtron into new component

--- src/App.js
+++ src/App.js
@@ -53,6 +53,79 @@ class MyProvider extends Component {
   }
 }
 
+const Weather = () => {
+  return (
+    <Fragment>
+      <MyContext.Consumer>
+        {context => (
+          <Fragment>
+            {context.weather.map(data => (
+              <Fragment key={data.id}>
+                <div className='jumbotron card card-image jumboTronbg mt-4'>
+                  <div className='text-white'>
+                    <h1 className='card-title h1-responsive mb-5 font-bold text-center'>
+                      Weather information from the{' '}
+                      <a href='https://openweathermap.org/'>OpenWeather API</a>
+                    </h1>
+                    <div className='row'>
+                      <div className='col-md-6'>
+                        <ul>
+                          <li>City Name: {data.name}</li>
+                          <li>Current Longitude: {data.coord.lon}</li>
+                          <li>Current Latitude: {data.coord.lat}</li>
+                          <li>Current Temp: {data.main.temp} °F</li>
+                          <li>Feels Like Temp: {data.main.feels_like} °F</li>
+                          <li>Minimum Temp: {data.main.temp_min} °F</li>
+                          <li>Maximum Temp: {data.main.temp_max} °F</li>
+                          <li>Wind Speed: {data.wind.speed} MPH</li>
+                          <li>Wind Direction: {data.wind.deg} °</li>
+                          <li>
+                            Wind Gust: {data.wind.gust}{' '}
+                            <i className='fas fa-wind' />
+                          </li>
+                        </ul>
+                      </div>
+                      <div className='col-md-6'>
+                        <ul>
+                          <li>
+                            Sunrise: <Moment unix>{data.sys.sunrise}</Moment>{' '}
+                          </li>
+                          <li>
+                            Sunset: <Moment unix>{data.sys.sunset}</Moment>{' '}
+                          </li>
+                          <li>
+                            Humidity: {data.main.humidity}{' '}
+                            <i className='fas fa-percentage' />
+                          </li>
+                          <li>
+                            Atmospheric Pressure: {data.main.pressure} hPa
+                          </li>
+                          <li>Visibility: {data.visibility * 0.0006} miles</li>
+                          <li>
+                            Cloudiness: {data.clouds.all}{' '}
+                            <i className='fas fa-percentage' />
+                          </li>
+                          <li>
+                            Weather Condition: {data.weather[0].description}
+                            <img
+                              src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                              alt='weather condition'
+                            />
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Fragment>
+            ))}
+          </Fragment>
+        )}
+      </MyContext.Consumer>
+    </Fragment>
+  );
+};
+
 const App = () => {
   return (
     <Fragment>
@@ -72,77 +145,7 @@ const App = () => {
                     Current Weather
                   </button>
                 </div>
-                {context.weather.map(data => (
-                  <Fragment key={data.id}>
-                    <div className='jumbotron card card-image jumboTronbg mt-4'>
-                      <div className='text-white'>
-                        <div>
-                          <h1 className='card-title h1-responsive mb-5 font-bold text-center'>
-                            Weather information from the{' '}
-                            <a href='https://openweathermap.org/'>
-                              OpenWeather API
-                            </a>
-                          </h1>
-                          <div className='row'>
-                            <div className='col-md-6'>
-                              <ul>
-                                <li>City Name: {data.name}</li>
-                                <li>Current Longitude: {data.coord.lon}</li>
-                                <li>Current Latitude: {data.coord.lat}</li>
-                                <li>Current Temp: {data.main.temp} °F</li>
-                                <li>
-                                  Feels Like Temp: {data.main.feels_like} °F
-                                </li>
-                                <li>Minimum Temp: {data.main.temp_min} °F</li>
-                                <li>Maximum Temp: {data.main.temp_max} °F</li>
-                                <li>Wind Speed: {data.wind.speed} MPH</li>
-                                <li>Wind Direction: {data.wind.deg} °</li>
-                                <li>
-                                  Wind Gust: {data.wind.gust}{' '}
-                                  <i className='fas fa-wind' />
-                                </li>
-                              </ul>
-                            </div>
-                            <div className='col-md-6'>
-                              <ul>
-                                <li>
-                                  Sunrise:{' '}
-                                  <Moment unix>{data.sys.sunrise}</Moment>{' '}
-                                </li>
-                                <li>
-                                  Sunset:{' '}
-                                  <Moment unix>{data.sys.sunset}</Moment>{' '}
-                                </li>
-                                <li>
-                                  Humidity: {data.main.humidity}{' '}
-                                  <i className='fas fa-percentage' />
-                                </li>
-                                <li>
-                                  Atmospheric Pressure: {data.main.pressure} hPa
-                                </li>
-                                <li>
-                                  Visibility: {data.visibility * 0.0006} miles
-                                </li>
-                                <li>
-                                  Cloudiness: {data.clouds.all}{' '}
-                                  <i className='fas fa-percentage' />
-                                </li>
-                                <li>
-                                  Weather Condition:{' '}
-                                  {data.weather[0].description}
-                                  <img
-                                    src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                                    alt='weather condition'
-                                  />
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Fragment>
-                ))}
+                <Weather />
               </div>
             </Fragment>
           )}
