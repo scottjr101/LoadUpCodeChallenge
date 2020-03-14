54042747580c63ebf2db67bb4405ea57a47a221d
Scott Ladd <scottjr101@gmail.com>
2020-03-14 00:55:42 -0400

    header component made into its own file

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const Header = () => {
+  return (
+    <Fragment>
+      <div className='container-fluid'>
+        <h1 className='py-4 text-center customFont m-0'>
+          LoadUp Engineering Coding Challenge
+        </h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
