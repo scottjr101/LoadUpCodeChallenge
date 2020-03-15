15c6305f011a59b797e1cb1941019525934202c4
Scott Ladd <scottjr101@gmail.com>
2020-03-15 15:02:39 -0400

    converted to useState and useEffect

--- src/components/MyProvider.js
+++ src/components/MyProvider.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
 // first we will make a new context
-const MyContext = React.createContext();
+const MyContext = createContext();
 
 // Then create a provider Component
-class MyProvider extends Component {
-  state = {
-    latitude: null,
-    longitude: null,
-    weather: [],
-    error: null
+const MyProvider = props => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [setError] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (toggle === false) {
+      console.log('effect 1');
+    } else {
+      window.navigator.geolocation.getCurrentPosition(success => {
+        setLatitude(success.coords.latitude);
+        setLongitude(success.coords.longitude);
+      });
+    }
+  }, [toggle, setLatitude, setLongitude]);
+
+  useEffect(() => {
+    if (longitude === null) {
+      console.log('effect 2');
+    } else {
+      fetch(
+        'https://api.openweathermap.org/data/2.5/weather?lat=' +
+          latitude +
+          '&lon=' +
+          longitude +
+          '&units=imperial&appid=1eab387463d70063b7d8296fa6d64c9b'
+      )
+        .then(res => res.json())
+        .then(result => setWeather([result]))
+        .catch(error => setError(error));
+    }
+  }, [latitude, longitude, setWeather, setError]);
+
+  const onClick = () => {
+    setToggle(true)
   };
 
-  render() {
-    return (
-      <MyContext.Provider
-        value={{
-          weather: this.state.weather,
-          getLocation: () => {
-            if (!window.navigator.geolocation) {
-              alert('Geolocation is not supported by your browser');
-            } else {
-              window.navigator.geolocation.getCurrentPosition(success => {
-                this.setState({
-                  latitude: success.coords.latitude,
-                  longitude: success.coords.longitude
-                });
-                fetch(
-                  'https://api.openweathermap.org/data/2.5/weather?lat=' +
-                    this.state.latitude +
-                    '&lon=' +
-                    this.state.longitude +
-                    '&units=imperial&appid=1eab387463d70063b7d8296fa6d64c9b'
-                )
-                  .then(res => res.json())
-                  .then(result => {
-                    this.setState({
-                      weather: [result]
-                    });
-                  })
-                  .catch(error => this.setState({ error }));
-              });
-            }
-          }
-        }}
-      >
-        {this.props.children}
-      </MyContext.Provider>
-    );
-  }
-}
+  return (
+    <MyContext.Provider
+      value={{
+        getLocation: onClick,
+        weather
+      }}
+    >
+      {props.children}
+    </MyContext.Provider>
+  );
+};
 
 export { MyContext, MyProvider };
