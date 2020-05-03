06442b1a518281a5daa295446e16dc2fc8cc7ec3
Scott Ladd <scottjr101@gmail.com>
2020-05-03 02:38:33 -0400

    redid coord button and form button layout

--- src/components/layout/CurrentWeather.js
+++ src/components/layout/CurrentWeather.js
@@ -1,44 +1,68 @@
 import React, { Fragment } from 'react';
 import { MyContext } from '../context/MyProvider';
+import chart from '../../assets/img/forecast-chart.png';
 
 const CurrentWeather = () => {
   return (
     <MyContext.Consumer>
       {(context) => (
         <Fragment>
-          {/* Card */}
-          <div className='card card-image cardbg'>
-            {/* Content */}
-            <div className='text-white text-center align-items-center rgba-black-slight py-4 px-4'>
-              <div>
-                <h2 className='mb-2'>
-                  Click the button below to the get weather imformation for your
-                  area
-                </h2>
-                <button
-                  className='btn btn-light-blue'
-                  onClick={context.getLocation}
-                >
-                  By Lat & Long
-                </button>
-                <form onSubmit={e => context.onSubmit(e)}>
-                  <input
-                    type='zipcode'
-                    value={context.zipcode}
-                    placeholder='30345'
-                    minLength='5'
-                    onChange={e => context.onChange(e)}
-                  />
-                  <input
-                    type='submit'
-                    value='Zipcode'
-                    className='btn btn-secondary'
-                  />
-                </form>
+          <div className='m-1 m-lg-5'>
+            <section className='dark-grey-text p-5'>
+              <div className='row'>
+                {/* img */}
+                <div className='col-lg-5 mb-4 mb-md-0'>
+                  <div className='view'>
+                    <img src={chart} className='img-fluid' alt='sample' />
+                  </div>
+                </div>
+                {/* img end */}
+                <div className='col-lg-7 mb-lg-0 mb-4'>
+                  <h3 className='font-weight-bold my-3'>
+                    Weather in your area
+                  </h3>
+                  <p className='text-muted mb-4 pb-2'>
+                    Click the "Coordinates" button to allow your browser to
+                    access your geolocation and grab the latitude and longitude
+                    needed. You will need to allow this site access to your
+                    location to preform this, if you uncomfortable with this.
+                    Please enter your zipcode in the input field below.
+                  </p>
+                  <div className='d-inline d-md-flex justify-content-between'>
+                    <button
+                      className='btn btn-md btn-light-blue mt-0 mx-0 mb-3'
+                      onClick={context.getLocation}
+                    >
+                      Coordinates
+                    </button>
+                    <form onSubmit={(e) => context.onSubmit(e)}>
+                      <div className='input-group'>
+                        <input
+                          type='zipcode'
+                          value={context.zipcode}
+                          placeholder='30345'
+                          minLength='5'
+                          onChange={(e) => context.onChange(e)}
+                        />
+                        <div className='input-group-append'>
+                          <button
+                            className='btn btn-md btn-secondary rounded-right m-0 px-3 py-2 z-depth-0 waves-effect'
+                            type='submit'
+                            value='Zipcode'
+                          >
+                            Enter
+                          </button>
+                        </div>
+                      </div>
+                      <small className='form-text black-text'>
+                        <strong>* Enter a vaild US 5-digit zipcode.</strong>
+                      </small>
+                    </form>
+                  </div>
+                </div>
               </div>
-            </div>
+            </section>
           </div>
-          {/* Card */}
         </Fragment>
       )}
     </MyContext.Consumer>
