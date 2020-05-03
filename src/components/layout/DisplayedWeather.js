84324e9c2bab633e855d65a68cdef9ad97c15586
Scott Ladd <scottjr101@gmail.com>
2020-05-03 02:37:51 -0400

    added background color

--- src/components/layout/DisplayedWeather.js
+++ src/components/layout/DisplayedWeather.js
@@ -10,99 +10,103 @@ const Weather = () => {
           <Fragment>
             {context.weather.map((data) => (
               <Fragment key={data.id}>
-                <div className='card p-4 weatherbg mt-4'>
-                  <div className='text-white'>
-                    <h1 className='card-title h1-responsive mb-5 font-bold text-center'>
-                      Weather information from the{' '}
-                      <a href='https://openweathermap.org/'>OpenWeather API</a>
-                    </h1>
-                    <div className='row'>
-                      <div className='col-md-6'>
-                        <ul>
-                          <li>City Name: {data.name}</li>
-                          <li>Current Longitude: {data.coord.lon}</li>
-                          <li>Current Latitude: {data.coord.lat}</li>
-                          <li>Current Temp: {data.main.temp} °F</li>
-                          <li>Feels Like Temp: {data.main.feels_like} °F</li>
-                          <li>Minimum Temp: {data.main.temp_min} °F</li>
-                          <li>Maximum Temp: {data.main.temp_max} °F</li>
-                          <li>Wind Speed: {data.wind.speed} MPH</li>
-                          <li>Wind Direction: {data.wind.deg} °</li>
-                          {data.wind.gust === undefined ? (
-                            <li>Wind Gust: N/A</li>
-                          ) : (
+                <div className='m-1 m-lg-5'>
+                  <div className='card p-4 cyan darken-2 mt-4'>
+                    <div className='text-white'>
+                      <h1 className='card-title h1-responsive mb-5 font-bold text-center'>
+                        Weather information from the{' '}
+                        <a href='https://openweathermap.org/'>
+                          OpenWeather API
+                        </a>
+                      </h1>
+                      <div className='row'>
+                        <div className='col-md-6'>
+                          <ul>
+                            <li>City Name: {data.name}</li>
+                            <li>Current Longitude: {data.coord.lon}</li>
+                            <li>Current Latitude: {data.coord.lat}</li>
+                            <li>Current Temp: {data.main.temp} °F</li>
+                            <li>Feels Like Temp: {data.main.feels_like} °F</li>
+                            <li>Minimum Temp: {data.main.temp_min} °F</li>
+                            <li>Maximum Temp: {data.main.temp_max} °F</li>
+                            <li>Wind Speed: {data.wind.speed} MPH</li>
+                            <li>Wind Direction: {data.wind.deg} °</li>
+                            {data.wind.gust === undefined ? (
+                              <li>Wind Gust: N/A</li>
+                            ) : (
+                              <li>
+                                Wind Gust: {data.wind.gust}{' '}
+                                <i className='fas fa-wind' />
+                              </li>
+                            )}
+                          </ul>
+                        </div>
+                        <div className='col-md-6'>
+                          <ul>
                             <li>
-                              Wind Gust: {data.wind.gust}{' '}
-                              <i className='fas fa-wind' />
+                              Sunrise:{' '}
+                              <Moment format='LLLL' unix>
+                                {data.sys.sunrise}
+                              </Moment>
                             </li>
-                          )}
-                        </ul>
-                      </div>
-                      <div className='col-md-6'>
-                        <ul>
-                          <li>
-                            Sunrise:{' '}
-                            <Moment format='LLLL' unix>
-                              {data.sys.sunrise}
-                            </Moment>
-                          </li>
-                          <li>
-                            Sunset:{' '}
-                            <Moment format='LLLL' unix>
-                              {data.sys.sunset}
-                            </Moment>
-                          </li>
-                          <li>
-                            Humidity: {data.main.humidity}{' '}
-                            <i className='fas fa-percentage' />
-                          </li>
-                          <li>
-                            Atmospheric Pressure: {data.main.pressure} hPa
-                          </li>
-                          {data.visibility === undefined ? (
-                            <li>Visibility: N/A</li>
-                          ) : (
                             <li>
-                              Visibility: {Math.round(data.visibility * 0.0006)}{' '}
-                              miles
+                              Sunset:{' '}
+                              <Moment format='LLLL' unix>
+                                {data.sys.sunset}
+                              </Moment>
                             </li>
-                          )}
-                          <li>
-                            Cloudiness: {data.clouds.all}{' '}
-                            <i className='fas fa-percentage' />
-                          </li>
-                          <li>
-                            Weather Condition: {data.weather[0].description}
-                          </li>
-                          <img
-                            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                            alt='weather condition'
-                          />
-                          {data.weather[0].main === 'Rain' ? (
                             <li>
-                              Rainfall in the past hour:{' '}
-                              {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
-                                1000}{' '}
-                              inches
+                              Humidity: {data.main.humidity}{' '}
+                              <i className='fas fa-percentage' />
                             </li>
-                          ) : data.weather[0].main === 'Thunderstorm' ? (
                             <li>
-                              Rainfall in the past hour:{' '}
-                              {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
-                                1000}{' '}
-                              inches
+                              Atmospheric Pressure: {data.main.pressure} hPa
                             </li>
-                          ) : data.weather[0].main === 'Drizzle' ? (
+                            {data.visibility === undefined ? (
+                              <li>Visibility: N/A</li>
+                            ) : (
+                              <li>
+                                Visibility:{' '}
+                                {Math.round(data.visibility * 0.0006)} miles
+                              </li>
+                            )}
                             <li>
-                              Rainfall in the past hour:{' '}
-                              {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
-                                1000}{' '}
-                              inches
+                              Cloudiness: {data.clouds.all}{' '}
+                              <i className='fas fa-percentage' />
                             </li>
-                          ) : (
-                            <p></p>
-                          )}
-                        </ul>
+                            <li>
+                              Weather Condition: {data.weather[0].description}
+                            </li>
+                            <img
+                              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                              alt='weather condition'
+                            />
+                            {data.weather[0].main === 'Rain' ? (
+                              <li>
+                                Rainfall in the past hour:{' '}
+                                {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
+                                  1000}{' '}
+                                inches
+                              </li>
+                            ) : data.weather[0].main === 'Thunderstorm' ? (
+                              <li>
+                                Rainfall in the past hour:{' '}
+                                {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
+                                  1000}{' '}
+                                inches
+                              </li>
+                            ) : data.weather[0].main === 'Drizzle' ? (
+                              <li>
+                                Rainfall in the past hour:{' '}
+                                {Math.ceil(data.rain['1h'] * 0.0393 * 1000) /
+                                  1000}{' '}
+                                inches
+                              </li>
+                            ) : (
+                              <p></p>
+                            )}
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </div>
